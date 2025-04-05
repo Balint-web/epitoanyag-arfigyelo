c2c4215945bdf43320beb2d5a1d1af819e7e220e
Balint-web <dreamgoods36@gmail.com.com>
2025-04-05 02:35:13 +0200

    ✨ Mobil optimalizálás és UI finomítás: középre igazítás, lekerekítések

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,57 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { useAuth } from "./Context";
-import { FaHome, FaShoppingCart, FaHeart, FaUser } from 'react-icons/fa'; // Ikonok importálása
+import { FaBars, FaHome, FaShoppingCart, FaHeart, FaUser } from 'react-icons/fa';
 import "./Navbar.css";
 
 const Navbar = () => {
-  const { user, logoutUser } = useAuth();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <div className="navbar-container">
-      {/* Legjobb árak szöveg */}
-      <div className="logo">
-             </div>
-      <ul className="nav-links">
-        <li>
-          <NavLink to="/">
-            <FaHome /> Főoldal
-          </NavLink>
-        </li>
-	<li>
-  		<NavLink to="/login">
-    		👤Belépés / Regisztráció
-  		</NavLink>
-	</li>
-        <li>
-          	<NavLink to="/termekek">
-            	<FaShoppingCart /> Termék figyelő
-          </NavLink>
-        </li>
-        <li>
-          	<NavLink to="/kapcsolat">
-            	<FaHeart /> Kapcsolatok
-          </NavLink>
-        </li>
-        <li>
-         	 <NavLink to="/kosar">
-           	 🛒 Kosár
-          </NavLink>
-        </li>
-        <li>
-          	<NavLink to="/kedvencek">
-           	❤️ Kedvencek
-          </NavLink>
-        </li>
-	<li>
-  		<NavLink to="/gyik">
-    		❓ GYIK
- 	 </NavLink>
-	</li>
-	
-      </ul>
+    <>
+      {/* 🌐 Asztali nézet */}
+      <div className="navbar-container desktop-only">
+        <ul className="nav-links">
+          <li><NavLink to="/"><FaHome /> Főoldal</NavLink></li>
+          <li><NavLink to="/login">👤 Belépés / Regisztráció</NavLink></li>
+          <li><NavLink to="/termekek"><FaShoppingCart /> Termék figyelő</NavLink></li>
+          <li><NavLink to="/kapcsolat"><FaHeart /> Kapcsolatok</NavLink></li>
+          <li><NavLink to="/kosar">🧵 Kosár</NavLink></li>
+          <li><NavLink to="/kedvencek">❤️ Kedvencek</NavLink></li>
+          <li><NavLink to="/gyik">❓ GYIK</NavLink></li>
+        </ul>
+      </div>
 
-    </div>
+      {/* 📱 Mobil nézet */}
+      <div className="bottom-navbar mobile-only">
+        <div className="nav-icons">
+          <NavLink to="/" className="nav-icon">
+            <FaHome />
+            <span>Főoldal</span>
+          </NavLink>
+          <NavLink to="/termekek" className="nav-icon">
+            <FaShoppingCart />
+            <span>Árfigyelő</span>
+          </NavLink>
+          <NavLink to="/kedvencek" className="nav-icon">
+            <FaHeart />
+            <span>Kedvencek</span>
+          </NavLink>
+          <NavLink to="/login" className="nav-icon">
+            <FaUser />
+            <span>Profil</span>
+          </NavLink>
+          <button onClick={toggleMenu} className="nav-icon hamburger-btn" style={{ fontSize: '1.4rem', background: 'none', border: 'none', color: 'white' }}>
+            <FaBars />
+            <span style={{ fontSize: '0.75rem' }}>Menü</span>
+          </button>
+        </div>
+
+        {menuOpen && (
+          <div className="dropdown-menu">
+            <NavLink to="/" onClick={toggleMenu}>🏠 Főoldal</NavLink>
+            <NavLink to="/termekek" onClick={toggleMenu}>🛒 Árfigyelő</NavLink>
+            <NavLink to="/kosar" onClick={toggleMenu}>🧵 Kosár</NavLink>
+            <NavLink to="/kedvencek" onClick={toggleMenu}>❤️ Kedvencek</NavLink>
+            <NavLink to="/gyik" onClick={toggleMenu}>❓ GYIK</NavLink>
+            <NavLink to="/kapcsolat" onClick={toggleMenu}>📞 Kapcsolat</NavLink>
+            <NavLink to="/login" onClick={toggleMenu}>🔐 Bejelentkezés / Regisztráció</NavLink>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
